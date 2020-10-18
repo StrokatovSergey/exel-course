f6e7d0ec97c7d283fdec96bd70744e0593f31e28
StrokatovSergey <s.strokatov@mail.ru>
2020-10-18 19:54:08 +0300

    resize | frontend | column resize is optimized bit row resze work incorrectly

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,15 +21,21 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]')
       const coords = $parent.getCoords()
       const type = $resizer.data.resize
+      let value
+
+      $resizer.css({
+        opacity: 1,
+        zIndex: '1000',
+        height: '100vh'
+      })
 
       const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
       document.onmousemove = e => {
         if (type === 'col') {
           const delta = e.pageX - coords.right
-          const value = coords.width + delta
-          $parent.css({with: value + 'px'})
-          cells.forEach(el => el.style.width = value + 'px')
+          value = coords.width + delta
+          $resizer.css({right: -delta + 'px'})
         } else {
           const delta = e.pageY - coords.bottom
           const value = coords.height + delta
@@ -39,6 +45,17 @@ export class Table extends ExcelComponent {
 
       document.onmouseup = () => {
         document.onmousemove = null
+        document.onmouseup = null
+        if (type === 'col') {
+          cells.forEach(el => el.style.width = value + 'px')
+          $parent.css({width: value + 'px'})
+          $resizer.css({
+            opacity: '0',
+            zIndex: 'auto',
+            height: 'auto',
+            right: '0'
+          })
+        }
       }
     }
   }
